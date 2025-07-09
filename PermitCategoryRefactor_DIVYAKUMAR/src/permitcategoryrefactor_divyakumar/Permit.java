19ccd5e7cd12952db416d7ef4138229e0ee0f71a
Divyakumar Patel <divy.patel0703@gmail.com>
2025-07-08 20:24:33 -0400

    second attempt

---
+++ PermitCategoryRefactor_DIVYAKUMAR/src/permitcategoryrefactor_divyakumar/Permit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package permitcategoryrefactor_divyakumar;
+
+/**
+ *
+ * @author divy5
+ */
+public class Permit {
+    
+private String holderName;
+    private PermitCategory category;
+
+    public Permit(String holderName, PermitCategory category) {
+        this.holderName = holderName;
+        this.category = category;
+    }
+
+    public void displayPermit() {
+        System.out.println("Permit Holder: " + holderName);
+        System.out.println("Permit Category: " + category);
+    }
+
+    public static void main(String[] args) {
+        Permit permit1 = new Permit("Alice", PermitCategory.RESIDENTIAL);
+        Permit permit2 = new Permit("Bob", PermitCategory.DIVYAKUMAR_CATEGORY);
+
+
+        permit1.displayPermit();
+        System.out.println();
+        permit2.displayPermit();
+    }
+}
---
+++ PermitCategoryRefactor_DIVYAKUMAR/src/permitcategoryrefactor_divyakumar/PermitCategory.java
@@ -0,0 +1,13 @@
+package permitcategoryrefactor_divyakumar;
+
+/**
+ *
+ * @author divy5
+ */
+public enum PermitCategory {
+    PARKING,
+    RESIDENTIAL,
+    BUSINESS,
+    VISITOR,
+    DIVYAKUMAR_CATEGORY  // custom category using your first name
+}
